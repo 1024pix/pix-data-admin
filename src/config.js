0c3cbbec825375073d4de00452477afbc4f32b4f
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-05-31 14:28:21 +0200

    feat: validate env

---
+++ src/config.js
@@ -0,0 +1,29 @@
+import 'dotenv/config.js';
+import { env } from 'node:process';
+import Joi from 'joi';
+
+const requiredEnvVars = Joi.object({
+  PORT: Joi.number().default(3000),
+  COOKIE_SECRET: Joi.string().min(32).required(),
+  ADMINJS_EMAIL: Joi.string().required(),
+  ADMINJS_PASSWORD: Joi.string().required(),
+})
+
+function validateEnvVars() {
+  const { error } = requiredEnvVars.validate(env, { allowUnknown: true });
+  if (error) {
+    throw new Error(`Environment variable validation error: ${error.message}`);
+  }
+}
+
+validateEnvVars();
+
+export const config = {
+  environment: env.NODE_ENV,
+  port: env.PORT,
+  cookieSecret: env.COOKIE_SECRET,
+  adminjs: {
+    email: env.ADMINJS_EMAIL,
+    password: env.ADMINJS_PASSWORD,
+  },
+};
