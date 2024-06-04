e2e906052b63740ffa1d5b15b01807a68a5221eb
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-06-04 11:21:09 +0200

    feat: add seeds

---
+++ seeders/20240603161846-demo-certification-centers.cjs
@@ -0,0 +1,21 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('certification-centers', [
+      {
+      name: 'Centre de certification de Pix',
+      externalId: 'PIXCENTER'
+      },
+      {
+      name: "Centre de l'équipe Data",
+      externalId: 'DATACENTER'
+      }
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('certification-centers', null, {});
+  }
+};
---
+++ seeders/20240604085131-demo-certification-centers-net-link.cjs
@@ -0,0 +1,19 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+
+    const certificationCenter = await queryInterface.sequelize.query("SELECT id FROM \"certification-centers\" WHERE name = 'Centre de certification de Pix'");
+    const certificationCentersNet = await queryInterface.sequelize.query("SELECT id FROM data_certification_centers_net WHERE name = 'Les centres de Pix'");
+
+    await queryInterface.bulkInsert('data_certification_centers_net_links', [{
+      certification_center_id: certificationCenter[0][0].id,
+      certification_centers_net_id: certificationCentersNet[0][0].id,
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('data_certification_centers_net_links', null, {});
+  }
+};
