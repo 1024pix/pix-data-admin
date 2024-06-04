fa9e1c91675ae3fcdd631d7150d68c2e85bf0c3e
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-06-04 11:21:02 +0200

    feat: add migrations

---
+++ migrations/20240604061813-create-certification-centers-net.cjs
@@ -0,0 +1,22 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('data_certification_centers_net', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('data_certification_centers_net');
+  }
+};
---
+++ models/certificationcenter.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class CertificationCenter extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  CertificationCenter.init({
+    name: DataTypes.STRING,
+    externalId: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'CertificationCenter',
+    tableName: 'certification-centers'
+  });
+  return CertificationCenter;
+};
