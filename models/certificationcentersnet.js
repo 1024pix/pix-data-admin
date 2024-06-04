466b5d1cca72249696f093d7105af95991d2d129
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-06-04 11:21:02 +0200

    feat: add migrations

---
+++ models/certificationcentersnet.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class CertificationCentersNet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  CertificationCentersNet.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'CertificationCentersNet',
+    tableName: 'data_certification_centers_net'
+  });
+  return CertificationCentersNet;
+};
