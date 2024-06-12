7c6f87db8355dbc6e7d4b3e00d21f88f81a38cd3
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-06-12 10:50:36 +0200

    feat: sync certification-center-net with ext

--- src/resources/certification-center-net.js
+++ src/resources/certification-center-net.js
@@ -1,5 +1,28 @@
-import { models } from '../sequelize.js';
+import { models, modelsExt } from '../sequelize.js';
+import { config } from '../config.js';
+import { creationHandler } from './creation-handler.js';
+
+async function create(params, t) {
+  const created = await models.CertificationCenterNet.create(params, { transaction: t });
+  if (config.externalIsActive) {
+    await modelsExt.CertificationCenterNet.create(params);
+  }
+  return created.toJSON();
+}
 
 export const certificationCenterNet = {
   resource: models.CertificationCenterNet,
+  options: {
+    actions: {
+      new: {
+        actionType: 'resource',
+        icon: 'Plus',
+        isAccessible: true,
+        handler: async (request, response, context) => {
+          return creationHandler(request, response, context, create);
+        },
+      },
+    },
+
+  }
 };
