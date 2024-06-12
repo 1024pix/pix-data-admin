c6e0b7bf80a7592f45e4e1d6daa407a0df18c920
Vincent Hardouin <vincent.hardouin@pix.fr>
2024-06-12 14:14:02 +0200

    feat: allow edit certification-center-net

--- src/resources/certification-center-net.js
+++ src/resources/certification-center-net.js
@@ -1,7 +1,8 @@
 import { models, modelsExt } from '../sequelize.js';
 import { config } from '../config.js';
 import { creationHandler } from './creation-handler.js';
-import {readOnlyOptions} from './utils.js';
+import { readOnlyOptions } from './utils.js';
+import { editHandler } from './edit-handler.js';
 
 async function create(params, t) {
   const created = await models.CertificationCenterNet.create(params, { transaction: t });
@@ -9,6 +10,26 @@ async function create(params, t) {
     await modelsExt.CertificationCenterNet.create(params);
   }
   return created.toJSON();
+}
+
+async function edit({ id, params, record, t }) {
+  await models.CertificationCenterNet.update(params, {
+    where: {
+      [record.resource.primaryKey()]: id,
+    },
+    individualHooks: true,
+    hooks: true,
+    transaction: t,
+  });
+  if (config.externalIsActive) {
+    await modelsExt.CertificationCenterNet.update(params, {
+      where: {
+        [record.resource.primaryKey()]: id,
+      },
+      individualHooks: true,
+      hooks: true,
+    });
+  }
 }
 
 export const certificationCenterNet = {
@@ -24,7 +45,14 @@ export const certificationCenterNet = {
           return creationHandler(request, response, context, create);
         },
       },
+      edit: {
+        actionType: 'record',
+        icon: 'Edit',
+        isAccessible: true,
+        handler: async (request, response, context) => {
+          return editHandler(request, response, context, edit);
+        },
+      },
     },
-
-  }
+  },
 };
